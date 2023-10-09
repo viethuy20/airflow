a9323a098d44a8140907fc0a36229857293542d0
viethuy20 <tranviethuy197@gmail.com>
2023-10-09 20:56:20 +0700

    first1

---
+++ dags/first.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+default_args={
+    "owner":"huy",
+    "retries":5,
+    "retry_delay" : timedelta(minutes=2)
+}
+with DAG(
+    dag_id = "first_dag",
+    default_args = default_args,
+    description="this is our first dag",
+    start_date=datetime(2023,9,19,2),
+    schedule_interval="@daily"
+ ) as dag:
+    task1 = BashOperator(
+        task_id = "first_task",
+        bash_command="echo hello work, this is first time"
+    )
+    task1
